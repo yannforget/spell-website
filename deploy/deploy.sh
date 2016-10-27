787ada6499251dc10d4d0543c638bac3ef9b1222
Yann <yann@forget.ch>
2016-10-27 18:22:23 +0200

    deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,25 +5,34 @@ elapsed=$(deploy/elapsed_time.py "deploy/last_deploy.txt")
 mintime=180
 if [ ! "$elapsed" > "$mintime" ]
 then
-   exit 0
+    echo "Time since last deployment: "$elapsed"s"
+    echo "Not enough elapsed time. Aborting..."
+    exit 0
 fi
 
 # Log new deploy
 date '+%m/%d/%Y %H:%M:%S' > deploy/last_deploy.txt
 
 # Pull from github
+echo "Pulling from github..."
 git pull
 sleep 1
 
 # Update publications
+echo "Updating publications from zotero..."
 rm -r content/publication
 mkdir -p content/publication
 deploy/fetch_zotero.py content/publication
 
 # Compile website
+echo "Compiling website with hugo..."
 hugo
 
 # Mount remote FTP, synchronize data and unmount
-echo $SPELL_PASSWORD | sshfs mgilbert@resu5.ulb.ac.be:/home/web1301/public_html spell.ulb.be -o password_stdin,allow_other
+echo "Mounting spell.ulb.be FTP..."
+source deploy/password.sh
+echo $password | sshfs mgilbert@resu5.ulb.ac.be:/home/web1301/public_html spell.ulb.be -o password_stdin,allow_other
+echo "Syncing files..."
 rsync -rz --delete public/ spell.ulb.be/
 fusermount -u spell.ulb.be
+echo "Done."
