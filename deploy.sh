9a20a700a7016a538639bc3bb2a2725e073e5172
Yann Forget <yannforget@mailbox.org>
2018-09-06 14:06:28 +0200

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Mirror local changes to a remote SFTP directory
+
+: ${FTP_HOSTNAME:?}
+: ${FTP_REMOTEDIR:?}
+: ${FTP_USERNAME:?}
+: ${FTP_PASSWORD:?}
+
+LOCALDIR="$(cd ../public && pwd)"
+
+lftp -c "open sftp://$FTP_HOSTNAME
+user $FTP_USERNAME '$FTP_PASSWORD'
+mirror --only-newer --reverse --delete --verbose $LOCALDIR $FTP_REMOTEDIR
+"
