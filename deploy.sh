452640396528b8b20d2c5fa8d70f5069fe73ae0c
Yann Forget <yannforget@mailbox.org>
2020-04-14 16:05:09 +0200

    Connect to ULB VPN before deploying

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# Mirror local changes to a remote SFTP directory
 
+# Connect to ULB VPN
+: ${ULB_NETID}
+: ${ULB_PASSWD}
+echo ${ULB_PASSWD} | openconnect --protocol=gp --user=$ULB_NETID --passwd-on-stdin vpn.ulb.ac.be
+
+# Mirror local changes to a remote SFTP directory
 : ${FTP_HOSTNAME:?}
 : ${FTP_REMOTEDIR:?}
 : ${FTP_USERNAME:?}
 : ${FTP_PASSWORD:?}
-
 LOCALDIR="$(cd public && pwd)"
-
 lftp -c "set sftp:auto-confirm yes; open sftp://$FTP_HOSTNAME
 user $FTP_USERNAME '$FTP_PASSWORD'
 mirror --only-newer --ignore-time --reverse --delete --verbose $LOCALDIR $FTP_REMOTEDIR
