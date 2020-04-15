414f652b82fe2ac33a3cf3312d0303782d481d6f
Yann Forget <yannforget@mailbox.org>
2020-04-15 15:19:46 +0200

    Connect to ULB VPN before deploying

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Connect to ULB VPN
-: ${ULB_NETID}
-: ${ULB_PASSWD}
-echo ${ULB_PASSWD} | openconnect --protocol=gp --user=$ULB_NETID --passwd-on-stdin vpn.ulb.ac.be
+echo $ULB_PASSWD | sudo openconnect --protocol=gp --user=$ULB_NETID --passwd-on-stdin vpn.ulb.ac.be &
+sleep 3
 
 # Mirror local changes to a remote SFTP directory
-: ${FTP_HOSTNAME:?}
-: ${FTP_REMOTEDIR:?}
-: ${FTP_USERNAME:?}
-: ${FTP_PASSWORD:?}
 LOCALDIR="$(cd public && pwd)"
 lftp -c "set sftp:auto-confirm yes; open sftp://$FTP_HOSTNAME
 user $FTP_USERNAME '$FTP_PASSWORD'
 mirror --only-newer --ignore-time --reverse --delete --verbose $LOCALDIR $FTP_REMOTEDIR
 "
+
+# Close VPN
+sudo killall openconnect
