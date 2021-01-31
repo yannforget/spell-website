38dedafe1b7dd40b20a751d581572a43c1177db1
Yann Forget <yannforget@mailbox.org>
2021-01-31 16:48:07 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,17 @@
 
 # Connect to ULB VPN
 echo $ULB_PASSWD | sudo openconnect --protocol=gp --background --user=$ULB_NETID --passwd-on-stdin vpn.ulb.ac.be 
-sleep 10
+sleep 5
+
+if ping -c 1 $FTP_HOSTNAME &> /dev/null
+then
+  echo "Connection to $FTP_HOSTNAME successfull"
+else
+  echo "Connection to $FTP_HOSTNAME failed"
+fi
 
 # Mirror local changes to a remote SFTP directory
+echo "Uploading changes..."
 LOCALDIR="$(cd public && pwd)"
 lftp -c "set sftp:auto-confirm yes; open sftp://$FTP_HOSTNAME
 user $FTP_USERNAME '$FTP_PASSWORD'
