a15cfd5d9a8e0dd716ce73ef7b2cfc9d23cc52d5
Yann Forget <yannforget@mailbox.org>
2021-02-04 17:07:04 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,6 @@
 echo $ULB_PASSWD | sudo openconnect --protocol=gp --background --user=$ULB_NETID --passwd-on-stdin vpn.ulb.ac.be 
 sleep 5
 
-mkdir -p ~/.ssh
-touch ~/.ssh/known_hosts
-ssh-keyscan -H $FTP_HOSTNAME >> ~/.ssh/known_hosts
-
 if ping -c 1 $FTP_HOSTNAME &> /dev/null
 then
   echo "Connection to $FTP_HOSTNAME successfull"
@@ -18,7 +14,7 @@ fi
 # Mirror local changes to a remote SFTP directory
 echo "Uploading changes..."
 LOCALDIR="$(cd public && pwd)"
-lftp -c "set sftp:auto-confirm yes; open sftp://$FTP_HOSTNAME
+lftp -c "set ssl:verify-certificate false; set sftp:auto-confirm yes; open sftp://$FTP_HOSTNAME
 user $FTP_USERNAME '$FTP_PASSWORD'
 mirror --only-newer --ignore-time --reverse --delete --verbose $LOCALDIR $FTP_REMOTEDIR
 "
