7a9417d862e4c346e0c9a29f9899d0404675ba0d
Yann Forget <yannforget@mailbox.org>
2021-02-09 11:48:13 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ ssh-keyscan -H $FTP_HOSTNAME >> ~/.ssh/known_hosts
 # Mirror local changes to a remote SFTP directory
 echo "Uploading changes..."
 LOCALDIR="$(cd public && pwd)"
-lftp -c "set ftp:ssl-allow yes; set ssl:verify-certificate false; set sftp:auto-confirm yes; open sftp://$FTP_HOSTNAME
+unbuffer lftp -c "set ftp:ssl-allow yes; set ssl:verify-certificate false; set sftp:auto-confirm yes; open sftp://$FTP_HOSTNAME
 user $FTP_USERNAME '$FTP_PASSWORD'
 mirror --only-newer --ignore-time --reverse --delete --verbose $LOCALDIR $FTP_REMOTEDIR
 "
